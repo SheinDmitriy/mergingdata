a8d6a8baa4406efb023572dfb44f07b44b570a31
shein <msh86.02.18@rambler.ru>
2021-01-15 15:01:46 +0500

    Добавил список ключей

--- src/main/java/main/MyFileVisit.java
+++ src/main/java/main/MyFileVisit.java
@@ -5,8 +5,22 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MyFileVisit extends SimpleFileVisitor<Path> {
+
+    private static Map<String, Integer> merge_key= new HashMap<>();
+
+    static {
+        merge_key.put("mark01", 0);
+        merge_key.put("mark17", 0);
+        merge_key.put("mark23", 0);
+        merge_key.put("mark35", 0);
+        merge_key.put("markFV", 0);
+        merge_key.put("markFX", 0);
+        merge_key.put("markFT", 0);
+    }
 
     public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) throws IOException {
         String fileName = path.getFileName().toString();
@@ -22,9 +36,16 @@ public class MyFileVisit extends SimpleFileVisitor<Path> {
                     break;
             }
         } else {
-            System.out.println("У файла нет расширения");
+            System.out.println("У файла " + fileName + " нет расширения");
         }
 
         return FileVisitResult.CONTINUE;
+    }
+
+    private void parseZIP(Path path) {
+    }
+
+    private void parseCSV(Path path) {
+
     }
 }
