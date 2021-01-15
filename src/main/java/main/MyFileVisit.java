681df89cb9fe6fa55347e60c7cf284890e6be635
shein <msh86.02.18@rambler.ru>
2021-01-15 15:43:24 +0500

    Добавил обработку csv файлов. метод parseCSV

--- src/main/java/main/MyFileVisit.java
+++ src/main/java/main/MyFileVisit.java
@@ -2,10 +2,12 @@ package main;
 
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MyFileVisit extends SimpleFileVisitor<Path> {
@@ -17,9 +19,9 @@ public class MyFileVisit extends SimpleFileVisitor<Path> {
         merge_key.put("mark17", 0);
         merge_key.put("mark23", 0);
         merge_key.put("mark35", 0);
-        merge_key.put("markFV", 0);
-        merge_key.put("markFX", 0);
-        merge_key.put("markFT", 0);
+        merge_key.put("markfv", 0);
+        merge_key.put("markfx", 0);
+        merge_key.put("markft", 0);
     }
 
     public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) throws IOException {
@@ -45,7 +47,16 @@ public class MyFileVisit extends SimpleFileVisitor<Path> {
     private void parseZIP(Path path) {
     }
 
-    private void parseCSV(Path path) {
-
+    private void parseCSV(Path path) throws IOException{
+        List<String> lines = Files.readAllLines(path);
+        for (String s: lines) {
+            if(!s.startsWith("#")){
+                String[] date = s.split(",");
+                int count = merge_key.get(date[0].toLowerCase());
+                count = count + Integer.parseInt(date[1]);
+                merge_key.put(date[0].toLowerCase(),count);
+            }
+        }
+        System.out.println(merge_key);
     }
 }
