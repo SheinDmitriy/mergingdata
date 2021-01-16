ab087a19bd30d505c398e64992cb3516a6a6cb08
SheinDmitriy <dmitriy.shein86@gmail.com>
2021-01-16 12:48:54 +0500

    Другой комментарии

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -7,13 +7,15 @@ import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) {
-
+        // Задаем путь к файлам и запускаем метод осклейки данных
         Path path = Paths.get("merge_file");
         runMerge(path);
     }
 
+    // Метод для склейки данных
     private static void runMerge(Path path){
         try {
+            // Используем метод walkFileTree из java nio для обхода всех файлов в заданной дериктории
             Files.walkFileTree(path, new MyFileVisit());
         } catch (IOException e) {
             e.printStackTrace();
--- src/main/java/main/MyFileVisit.java
+++ src/main/java/main/MyFileVisit.java
@@ -31,6 +31,7 @@ public class MyFileVisit extends SimpleFileVisitor<Path> {
     private static ArrayList<Integer> array6 = new ArrayList<>();
     private static ArrayList<Integer> array7 = new ArrayList<>();
 
+    // HashMap с заданными ключями и массивами данных для обработки
     private static Map<String, ArrayList<Integer>> merge_key = new HashMap<>();
     static {
         merge_key.put("mark01", array1);
@@ -42,11 +43,14 @@ public class MyFileVisit extends SimpleFileVisitor<Path> {
         merge_key.put("markft", array7);
     }
 
+    // Метод который обходит и выболняет действия по очереди с каждым файлом в задвнной директории
     public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) throws IOException {
         String fileName = path.getFileName().toString();
         System.out.println("Обрабатываем файл: " + fileName);
 
+        // Выявляем расширение файла
         if(fileName.lastIndexOf(".") != -1 && fileName.lastIndexOf(".") != 0){
+            // В зависимости от расширения запускаем соответсвующий парсер данных
             switch (fileName.substring(fileName.lastIndexOf(".")+1)){
                 case "csv" :
                     parseCSV(path);
@@ -60,11 +64,13 @@ public class MyFileVisit extends SimpleFileVisitor<Path> {
         }
         return CONTINUE;
     }
-
+    // Метод для парсера данных из ZIP файлв
     private void parseZIP(Path path) throws IOException{
+        // Определяем zip файл как папку
         FileSystem fs = FileSystems.newFileSystem(path, null);
         Path zipPath = fs.getPath("/");
 
+        // Запускаем обход по "zip-папке"
         try {
             Files.walkFileTree(zipPath, new MyFileVisit());
         } catch (IOException e) {
@@ -72,13 +78,19 @@ public class MyFileVisit extends SimpleFileVisitor<Path> {
         }
     }
 
+    // Метод для прасинга данных из cvs файлов
     private void parseCSV(Path path) throws IOException{
+        // Считываем данные из файла в массив строк
         List<String> lines = Files.readAllLines(path);
+        // Обходим каждую строку отдельно
         for (String s: lines) {
             ArrayList<Integer> arrayTemp = new ArrayList<>();
+            // Проверяем закоментированна строка или нет
             if(!s.startsWith("#")){
+                // Разделяем строку на ключ и значение
                 String[] date = s.split(",");
                 String key = date[0].toLowerCase();
+                // Заполняем массивы в HashMap соответственно ключу
                 arrayTemp = merge_key.get(key);
                 arrayTemp.add(Integer.parseInt(date[1]));
                 merge_key.put(key,arrayTemp);
@@ -87,26 +99,35 @@ public class MyFileVisit extends SimpleFileVisitor<Path> {
         System.out.println(merge_key);
     }
 
+    // Метод для выполнения действий после завершения обхода директории
     @Override
     public FileVisitResult postVisitDirectory(Path path, IOException exc) throws IOException {
+        // Если закончили обход "zip-папки" то ничего не делаем, просто завершаем обход
         if(path.toString().equals("/")){
             return TERMINATE;
         }
+        // Обробатываем данные в массивах HashMap для формирования трех типов отчетов
         System.out.println("Формируем данные для отчета");
+        // Обходим каждый элемент HashMap
         merge_key.forEach((k, v) ->{
+            // Проверяем пустой ли массив
             if(!v.isEmpty()){
+                // Сортируем данные в массиве для 3 отчета
                 Collections.sort(v, Collections.reverseOrder());
-               jsonObjectV3.put(k,v);
-               int count = 0;
+                jsonObjectV3.put(k,v);
+                // Обходим массивы и складываем значения внутри для отчета 1 и 2
+                int count = 0;
                 for (Integer s: v) {
                     count = count + s;
                 }
                 jsonObjectV1.put(k, count);
                 jsonObjectV2.put(k, count);
             }else {
+                // Добавляем пустые массивы для отчета 2
                 jsonObjectV2.put(k, null);
             }
         });
+        // Записываем данные в три разных файла
         System.out.println(jsonObjectV3);
         jsonWriteFile(jsonObjectV3, "report №3.txt");
         System.out.println(jsonObjectV1.toString());
@@ -119,6 +140,7 @@ public class MyFileVisit extends SimpleFileVisitor<Path> {
         return TERMINATE;
     }
 
+    // Метод для записи данных в файл
     private void jsonWriteFile(JSONObject jsonObject, String filename) throws IOException{
         jsonfile = new FileWriter("report/" + filename);
         jsonfile.write(jsonObject.toJSONString());
