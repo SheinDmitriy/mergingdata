97f6c3c1396c492c69612245dc0bbee9f74f6c1c
SheinDmitriy <dmitriy.shein86@gmail.com>
2021-01-16 12:21:49 +0500

    Другой метод реализации

--- src/main/java/main/MyFileVisit.java
+++ src/main/java/main/MyFileVisit.java
@@ -8,9 +8,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static java.nio.file.FileVisitResult.CONTINUE;
 import static java.nio.file.FileVisitResult.TERMINATE;
@@ -25,15 +23,23 @@ public class MyFileVisit extends SimpleFileVisitor<Path> {
     private JSONObject jsonObjectV2 = new JSONObject();
     private JSONObject jsonObjectV3 = new JSONObject();
 
-    private static Map<String, Integer> merge_key = new HashMap<>();
+    private static ArrayList<Integer> array1 = new ArrayList<>();
+    private static ArrayList<Integer> array2 = new ArrayList<>();
+    private static ArrayList<Integer> array3 = new ArrayList<>();
+    private static ArrayList<Integer> array4 = new ArrayList<>();
+    private static ArrayList<Integer> array5 = new ArrayList<>();
+    private static ArrayList<Integer> array6 = new ArrayList<>();
+    private static ArrayList<Integer> array7 = new ArrayList<>();
+
+    private static Map<String, ArrayList<Integer>> merge_key = new HashMap<>();
     static {
-        merge_key.put("mark01", null);
-        merge_key.put("mark17", null);
-        merge_key.put("mark23", null);
-        merge_key.put("mark35", null);
-        merge_key.put("markfv", null);
-        merge_key.put("markfx", null);
-        merge_key.put("markft", null);
+        merge_key.put("mark01", array1);
+        merge_key.put("mark17", array2);
+        merge_key.put("mark23", array3);
+        merge_key.put("mark35", array4);
+        merge_key.put("markfv", array5);
+        merge_key.put("markfx", array6);
+        merge_key.put("markft", array7);
     }
 
     public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) throws IOException {
@@ -69,17 +75,16 @@ public class MyFileVisit extends SimpleFileVisitor<Path> {
     private void parseCSV(Path path) throws IOException{
         List<String> lines = Files.readAllLines(path);
         for (String s: lines) {
-            int count = 0;
+            ArrayList<Integer> arrayTemp = new ArrayList<>();
             if(!s.startsWith("#")){
                 String[] date = s.split(",");
                 String key = date[0].toLowerCase();
-                if( merge_key.get(key) != null) {
-                    count = merge_key.get(key);
-                }
-                count = count + Integer.parseInt(date[1]);
-                merge_key.put(key,count);
+                arrayTemp = merge_key.get(key);
+                arrayTemp.add(Integer.parseInt(date[1]));
+                merge_key.put(key,arrayTemp);
             }
         }
+        System.out.println(merge_key);
     }
 
     @Override
@@ -87,23 +92,29 @@ public class MyFileVisit extends SimpleFileVisitor<Path> {
         if(path.toString().equals("/")){
             return TERMINATE;
         }
-        System.out.println("Формируем данные для отчета №1");
+        System.out.println("Формируем данные для отчета");
         merge_key.forEach((k, v) ->{
-            if(v != null){
-                jsonObjectV1.put(k,v);
+            if(!v.isEmpty()){
+                Collections.sort(v, Collections.reverseOrder());
+               jsonObjectV3.put(k,v);
+               int count = 0;
+                for (Integer s: v) {
+                    count = count + s;
+                }
+                jsonObjectV1.put(k, count);
+                jsonObjectV2.put(k, count);
+            }else {
+                jsonObjectV2.put(k, null);
             }
         });
+        System.out.println(jsonObjectV3);
+        jsonWriteFile(jsonObjectV3, "report №3.txt");
         System.out.println(jsonObjectV1.toString());
         System.out.println("Формируем файл для отчета №1");
         jsonWriteFile(jsonObjectV1, "report №1.txt");
-
-        System.out.println("Формируем данные для отчета №2");
-        jsonObjectV2.putAll(merge_key);
         System.out.println(jsonObjectV2.toString());
         System.out.println("Формируем файл для отчета №2");
         jsonWriteFile(jsonObjectV2, "report №2.txt");
-
-        System.out.println("Формируем данные для отчета №3");
 
         return TERMINATE;
     }
